aa2a119563f8b943f53228863c8818ee5784f194
DevSadit <dev.sadit@gmail.com>
2024-06-02 23:34:10 +0600

    get post data

--- index.js
+++ index.js
@@ -12,7 +12,52 @@ app.use(
 );
 app.use(express.json());
 
-//
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.irm8dks.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const testimonialCollection = client
+      .db(`Fitness_Tracker`)
+      .collection(`testimonials`);
+    const postCollection = client.db(`Fitness_Tracker`).collection(`posts`);
+
+    //   get the testimonials from database
+    app.get(`/testimonials`, async (req, res) => {
+      const result = await testimonialCollection.find().toArray();
+      res.send(result);
+    });
+
+    //   get the posts from database
+    app.get(`/posts`, async (req, res) => {
+      const result = await postCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send(`fitnes tracker server is working!!`);
 });
